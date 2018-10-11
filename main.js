4435e59fd5718e562f7157633cfc711d4e0eddb5
abornstein <abornstein@altima-agency.com>
2018-10-11 19:03:27 +0200

    Storing Data in Component with data Method

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+var data = { status: 'Critical' };
+
+Vue.component('my-cmp', {
+  data: function() {
+    return data;
+  },
+  template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal'
+    }
+  }
+});
+
+new Vue({
+  el: '#app2'
+});
+
 new Vue({
 	el: '#app',
   data: {
