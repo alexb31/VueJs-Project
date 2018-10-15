1d28c53b467516d1631341684a6c7eeb95a5c865
abornstein <abornstein@altima-agency.com>
2018-10-15 16:36:44 +0200

    Validating Props

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,13 +2,21 @@
     <div class="component">
         <h3>You may enter the user details here</h3>
         <p>Many details</p>
-        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: {
+            myName: String,
+            default: 'Max'
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("")
+            }
+        }
     }
 </script>
 
