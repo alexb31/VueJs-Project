f9514d98141da9f3ade4b3160492854567e59b27
abornstein <abornstein@altima-agency.com>
2018-11-30 14:09:05 +0100

    Learned Forms in VueJS

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,26 @@
           <hr>
           <div class="form-group">
             <label for="email">Mail</label>
-            <input type="text" id="email" class="form-control" v-model="email">
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              :value="userData.email"
+              @input="userData.email = $event.target.value"
+            >
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" class="form-control">
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="userData.password"
+            >
           </div>
           <div class="form-group">
             <label for="age">Age</label>
-            <input type="number" id="age" class="form-control">
+            <input type="number" id="age" class="form-control" v-model="userData.age">
           </div>
         </div>
       </div>
@@ -24,17 +35,17 @@
           <label for="message">Message</label>
           <br>
           <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea id="message" rows="5" class="form-control"></textarea>
+          <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail"> Send Mail
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail"> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail"> Send Infomail
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail"> Send Infomail
             </label>
           </div>
         </div>
@@ -42,49 +53,54 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
-            <input type="radio" id="male" value="Male"> Male
+            <input type="radio" id="male" value="Male" v-model="gender"> Male
           </label>
           <label for="female">
-            <input type="radio" id="female" value="Female"> Female
+            <input type="radio" id="female" value="Female" v-model="gender"> Female
           </label>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="priority">Priority</label>
-          <select id="priority" class="form-control">
-            <option></option>
+          <select id="priority" class="form-control" v-model="selectedPriority">
+            <option v-for="priority in priorities">{{ priority }}</option>
           </select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <app-switch v-model="dataSwitch"></app-switch>
         </div>
       </div>
       <hr>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
       </div>
     </form>
     <hr>
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4>Your Data</h4>
           </div>
           <div class="panel-body">
-            <p>Mail: {{ email }}</p>
-            <p>Password:</p>
-            <p>Age:</p>
-            <p>Message:</p>
+            <p>Mail: {{ userData.email }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Age: {{ userData.age }}</p>
+            <p style="white-space: pre">Message: {{message}}</p>
             <p>
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+              <li v-for="item in sendMail">{{ item }}</li>
             </ul>
-            <p>Gender:</p>
-            <p>Priority:</p>
-            <p>Switched:</p>
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{ selectedPriority }}</p>
+            <p>Switched: {{ dataSwitch }}</p>
           </div>
         </div>
       </div>
@@ -93,11 +109,32 @@
 </template>
 
 <script>
+import Switch from './components/Switch.vue';
+
 export default {
   data() {
     return {
-      email: ""
+      userData: {
+        email: "",
+        password: "",
+        age: 27
+      },
+      message: "New Text",
+      sendMail: [],
+      gender: "Male",
+      selectedPriority: "High",
+      priorities: ["High", "Medium", "Low"],
+      dataSwitch: true,
+      isSubmitted: false
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
+  },
+  components: {
+    appSwitch: Switch
   }
 };
 </script>
